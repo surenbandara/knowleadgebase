56dec0280ca9fb0626a90b58c810a66cfb51860e
surenbandara7 <surenbandara7@gmail.com>
2024-04-19 11:25:24 +0530

    changed knoleadgebasemanager.py

--- KnowleaderbaseManager.py
+++ KnowleaderbaseManager.py
@@ -22,7 +22,6 @@ class KnowleaderbaseManager:
             embeddings = HuggingFaceBgeEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
                                                   model_kwargs={'device': 'cuda'})
         self.embeddings = embeddings
-        self.file_paths = self.create_file_paths(path)
 
         path_knoleadgebase_path = "pathkb.json"
         file_path_path  = "filepath.json"
@@ -33,6 +32,7 @@ class KnowleaderbaseManager:
           self.path_knowleadgebase = self.retrieve_data_from_json(path_knoleadgebase_path,self.embeddings)["allpaths"]["knowleadgebase"]
 
         except FileNotFoundError:
+          self.file_paths = self.create_file_paths(path)
           content_data = []
           for i in self.file_paths.keys():
               file_list = []
@@ -95,7 +95,8 @@ class KnowleaderbaseManager:
                         file_paths[root]['files'].append(filepath)
                     else:
                         file_paths[root] = {}
-                        file_paths[root]['content'] = root + "=" + filename
+                        # file_paths[root]['content'] = root + "=" + filename
+                        file_paths[root]['content'] = filename
                         file_paths[root]['files'] = [filepath]
 
         return file_paths
